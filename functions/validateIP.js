058c6be4a688264ecf5fe2e6acd0f62462814496
amirhossein lashkari <am.lashkari2017@gmail.com>
2024-09-22 14:05:38 +0330

    add validation of private ip in function

--- functions/validateIP.js
+++ functions/validateIP.js
@@ -1,7 +1,28 @@
-// Function to validate IP address
-const validateIP = (ip) => {
-    const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    return regex.test(ip);
-};
+const net = require('net');
+
+function isPrivate(ip) {
+    const parts = ip.split('.');
+    return parts[0] === '10' ||
+        (parts[0] === '0' && parts[1] === '0' && parts[2] === '0' && parts[3] === '0') ||
+        (parts[0] === '172' && parts[1] >= 16 && parts[1] <= 31) ||
+        (parts[0] === '192' && parts[1] === '168');
+}
+
+function validateIP(ipAddress) {
+    const isPrivateIp = isPrivate(ipAddress);
+
+    if (!net.isIP(ipAddress)) {
+        return { isValid: false, message: 'آیپی باید شامل اعداد انگلیسی باشد و فرمت ان صحیح باشد!' };
+    }
+
+    if (isPrivateIp) {
+        return { isValid: false, message: 'آیپی پرایویت مجاز نمیباشد!' };
+    }
+
+    return {
+        isValid: true,
+        message: 'آیپی صحیح میباشد.'
+    };
+}
 
 module.exports = { validateIP }
